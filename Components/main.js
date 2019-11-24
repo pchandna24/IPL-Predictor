1bfd1c7c5622da441ff9fedbee7d20736df75126
Parth Chandna <pchandna24@gmail.com>
2019-11-24 23:54:26 +0530

    Update main.js

--- Components/main.js
+++ Components/main.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { AppLoading } from 'expo';
-import { Text, View, Image,Picker} from 'react-native';
+import { Text, View,ScrollView, Image,Picker} from 'react-native';
 import undraw from '../assets/undraw.png';
 import axios from 'axios';
 
+const key = {
+    "Hyderabad":"SRH",
+    "Mumbai":"MI",
+    "Bangalore":"RCB",
+    "Kolkata":"KKR",
+    "Delhi":"DD",
+    "Chennai":"CSK",
+    "Rajasthan":"RR",
+}
 
 export default class Main extends Component {
     state={
@@ -13,51 +22,38 @@ export default class Main extends Component {
         venue:null,
         tosschoice:null,
         data:null,
-        ridx:Math.floor(Math.random() * 2)
+        tossVal:null
     }
 
-     meraDost =()=>{
+     meraDost =async()=>{
         this.setState({
             data:"Loading...."
         })
-        let arr = [this.state.team1, this.state.team2]
-        let resServer = arr[this.state.ridx]
+
         console.log(this.state.team1);
         console.log(this.state.team2);
         console.log(this.state.venue);
-        console.log(this.state.toss);
+        console.log(this.state.tossVal);
         console.log(this.state.tosschoice);
-        if(this.state.toss==1){
-            this.setState({
-                toss: this.state.team1
-            })
-        }
-        else if(this.state.toss==2){
-            this.setState({
-                toss:this.state.team2
-            })
-        }
-        setTimeout(()=>{
-            console.log(this.state.toss);
+
         let formData = new FormData()
         formData.append("homeTeam", this.state.team1)
-        formData.append("awayTeam", this.state.team2)
-        formData.append("city", this.state.venue)
-        formData.append("tossWinner", this.state.toss)
+        formData.append("awayTeam", this.state.team2 )
+        formData.append("city", `City: ${this.state.venue}`)
+        formData.append("tossWinner",  this.state.tossVal || "KKR")
         formData.append("selector1", this.state.tosschoice)
-        formData.append("probability",this.state.ridx)
 
             try{
-                let res =  resServer || axios.post('https://minor123456.herokuapp.com/submit',formData)
+                let res = await axios.post('https://minor123456.herokuapp.com/submit',formData)
                 console.log(res);
                 this.setState({
-                    data:res
+                    data:res.data.data
                 })
                 }
-            catch{
+            catch(err){
+                console.log(err)
                 console.log("err");
             }
-        }, 2000)
    
         
     }
@@ -65,7 +61,8 @@ export default class Main extends Component {
 
     render() {
         return (
-            <View style={{width:355,alignItems:'center',marginTop:40}}>
+            <ScrollView >
+                <View style={{width:355,alignItems:'center',marginTop:40}}>
                 <Image source={undraw} style={{height:240,width:355,resizeMode:'cover',borderRadius:25}}/>
                 <View style={{height:428,width:355,backgroundColor:'#fff',borderRadius:25,marginTop:12}}>
                     <View style={{display:'flex',flexDirection:'row'}}>
@@ -73,14 +70,16 @@ export default class Main extends Component {
                 <Picker
                     selectedValue={this.state.team1}
                     style={{ height: 50, width: 150,marginTop:5}}
-                    onValueChange={(itemValue, itemIndex) => this.setState({ team1: itemValue })}>
-                    <Picker.Item label="Sunrisers Hyderabad" value="SRH" />
-                    <Picker.Item label="Mumbai Indians" value="MI" />
-                    <Picker.Item label="Royal Challenger Bangalore" value="RCB" />
-                    <Picker.Item label="Kolkata Knight Riders" value="KKR" />
-                    <Picker.Item label="Delhi Daredevils" value="DR" />
-                    <Picker.Item label="Chennai Super Kings" value="CSK" />
-                    <Picker.Item label="Rajasthan Royals" value="RR" />
+                    onValueChange={team1 => this.setState({ team1 })}>
+     <Picker.Item label="Select Choice" value="Select Choice" />
+
+                    <Picker.Item label="Sunrisers Hyderabad" value="Hyderabad" />
+                    <Picker.Item label="Mumbai Indians" value="Mumbai" />
+                    <Picker.Item label="Royal Challenger Bangalore" value="Bangalore" />
+                    <Picker.Item label="Kolkata Knight Riders" value="Kolkata" />
+                    <Picker.Item label="Delhi Daredevils" value="Delhi" />
+                    <Picker.Item label="Chennai Super Kings" value="Chennai" />
+                    <Picker.Item label="Rajasthan Royals" value="Rajasthan" />
 
                 </Picker>
                     </View>
@@ -89,14 +88,16 @@ export default class Main extends Component {
                 <Picker
                     selectedValue={this.state.team2}
                     style={{ height: 50, width: 150,marginTop:5}}
-                    onValueChange={(itemValue, itemIndex) => this.setState({ team2: itemValue })}>
-                  <Picker.Item label="Sunrisers Hyderabad" value="SRH" />
-                    <Picker.Item label="Mumbai Indians" value="MI" />
-                    <Picker.Item label="Royal Challenger Bangalore" value="RCB" />
-                    <Picker.Item label="Kolkata Knight Riders" value="KKR" />
-                    <Picker.Item label="Delhi Daredevils" value="DR" />
-                    <Picker.Item label="Chennai Super Kings" value="CSK" />
-                    <Picker.Item label="Rajasthan Royals" value="RR" />
+                    onValueChange={team2 => this.setState({ team2 })}>
+                             <Picker.Item label="Select Choice" value="Select Choice" />
+
+ <Picker.Item label="Sunrisers Hyderabad" value="Hyderabad" />
+                    <Picker.Item label="Mumbai Indians" value="Mumbai" />
+                    <Picker.Item label="Royal Challenger Bangalore" value="Bangalore" />
+                    <Picker.Item label="Kolkata Knight Riders" value="Kolkata" />
+                    <Picker.Item label="Delhi Daredevils" value="Delhi" />
+                    <Picker.Item label="Chennai Super Kings" value="Chennai" />
+                    <Picker.Item label="Rajasthan Royals" value="Rajasthan" />
                     
                 </Picker>
                     </View>
@@ -105,28 +106,33 @@ export default class Main extends Component {
                 <Picker
                     selectedValue={this.state.venue}
                     style={{ height: 50, width: 150,marginTop:5}}
-                    onValueChange={(itemValue, itemIndex) => this.setState({ venue: itemValue })}>
-                    <Picker.Item label="Hyderabad" value="1" />
-                    <Picker.Item label="Pune" value="2" />
-                    <Picker.Item label="Rajkot" value="3" />
-                    <Picker.Item label="Indore" value="4" />
-                    <Picker.Item label="Bangalore" value="5" />
-                    <Picker.Item label="Mumbai" value="6" />
-                    <Picker.Item label="kolkata" value="7" />
-                    <Picker.Item label="Delhi" value="8" />
-                    <Picker.Item label="Jaipur" value="11" />
-                    <Picker.Item label="Chennai" value="12" />
+                    onValueChange={venue => this.setState({ venue })}>
+                             <Picker.Item label="Select Choice" value="Select Choice" />
+
+                    <Picker.Item label="Hyderabad" value="Hyderabad" />
+                    <Picker.Item label="Pune" value="Pune" />
+                    <Picker.Item label="Rajkot" value="Rajkot" />
+                    <Picker.Item label="Indore" value="Indore" />
+                    <Picker.Item label="Bangalore" value="Bangalore" />
+                    <Picker.Item label="Mumbai" value="Mumbai" />
+                    <Picker.Item label="kolkata" value="Kolkata" />
+                    <Picker.Item label="Delhi" value="Delhi" />
+                    <Picker.Item label="Jaipur" value="Jaipur" />
+                    <Picker.Item label="Chennai" value="Chennai" />
 
                 </Picker>
                     </View>
                     <View style={{display:'flex',flexDirection:'row'}}>
                     <Text style={{color:'#1A1A1A',width:150,fontWeight:'bold',fontSize:20,alignItems:'center',justifyContent:'center',margin:15,marginLeft:22}}>Toss Winner</Text>
                 <Picker
-                    selectedValue={this.state.toss}
+                    selectedValue={this.state.tossVal}
                     style={{ height: 40, width: 150,marginTop:5}}
-                    onValueChange={(itemValue, itemIndex) => this.setState({ toss: itemValue })}>
-                     <Picker.Item label="Team-1" value="1" />
-                     <Picker.Item label="Team-2" value="2" />
+                    onValueChange={tossVal => {
+                        this.setState({ tossVal })
+                    }}>
+                     <Picker.Item label="Select Choice" value="Select Choice" />
+                     <Picker.Item label="Team-1" value={key[this.state.team1]} />
+                     <Picker.Item label="Team-2" value={key[this.state.team2]} />
                 </Picker>
                     </View>
                     <View style={{display:'flex',flexDirection:'row'}}>
@@ -134,9 +140,11 @@ export default class Main extends Component {
                 <Picker
                     selectedValue={this.state.tosschoice}
                     style={{ height: 50, width: 150,marginTop:5}}
-                    onValueChange={(itemValue, itemIndex) => this.setState({ tosschoice: itemValue })}>
-                     <Picker.Item label="Field" value="1" />
-                    <Picker.Item label="Bat" value="2" />
+                    onValueChange={tosschoice => this.setState({ tosschoice })}>
+                             <Picker.Item label="Select Choice" value="Select Choice" />
+
+                     <Picker.Item label="Field" value="Field" />
+                    <Picker.Item label="Bat" value="Bat" />
                 </Picker>
                     </View>
 
@@ -149,8 +157,8 @@ export default class Main extends Component {
                 </View>
                 
 
-                
-            </View>
+                </View>
+            </ScrollView>
         )
     }
 }
